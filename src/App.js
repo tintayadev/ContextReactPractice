f74646dfe7844328221aee9d328f0f7e89c46d25
Paulo Tintaya <pau.tintaya.code@gamil.com>
2022-12-15 17:23:39 -0400

    Use of context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Username from './components/Username';
+
+
+export const UserContext = React.createContext(); 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value="Jesus">
+        <Username/>
+      </UserContext.Provider>
     </div>
   );
 }
+
+// function User() {
+//   const value = React.useContext(UserContext);
+//   return <h1>{value}</h1>;
+//   // return (
+//   //   <UserContext.Consumer>
+//   //     {value =>  <h1>{value}</h1>}
+//   //   </UserContext.Consumer>
+//   // )
+// }
 
 export default App;
